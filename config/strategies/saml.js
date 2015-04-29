d516de048efb0799afeb2e576233aca1fc9a0fb1
Robin Hansma <robinhansma@outlook.com>
2015-04-29 16:51:22 +0200

    Added possibility to login with surfconext

--- config/strategies/saml.js
+++ config/strategies/saml.js
@@ -12,13 +12,26 @@ module.exports = function() {
   passport.use(new SamlStrategy({
       path: '/login/callback',
       entryPoint: 'https://engine.surfconext.nl/authentication/idp/single-sign-on',
-      issuer: 'passport-saml'
+      issuer: 'https://engine.surfconext.nl/authentication/idp/metadata'
     },
     function(profile, done) {
-      User.findByStudentNumber(profile.studentNumber, function(err, user) {
+      User.findOne({
+        studentNumber: profile.studentNumber
+      }, function(err, user) {
         if (err) {
           return done(err);
         }
+        if (!user) {
+          return done(null, false, {
+            message: 'Unknown user or invalid password'
+          });
+        }
+        if (!user.authenticate(profile.password)) {
+          return done(null, false, {
+            message: 'Unknown user or invalid password'
+          });
+        }
+
         return done(null, user);
       });
     })
