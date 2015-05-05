4f66da7a3c9906a21aecf6895ad4ec05203cc2a8
Robin Hansma <robin@debsolutions.nl>
2015-05-05 20:49:25 +0200

    Added goals to menu and removed articles from menu

---
+++ public/modules/goals/config/goals.client.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Goals module config
+angular.module('goals').run(['Menus',
+	function(Menus) {
+    //Set topbar menu
+    Menus.addMenuItem('topbar', 'Goals', 'goals', 'dropdown', '/goals(/create)?');
+    Menus.addSubMenuItem('topbar', 'goals', 'List Goals', 'goals');
+    Menus.addSubMenuItem('topbar', 'goals', 'New Goal', 'goals/create');
+	}
+]);
