94f93748e41d21a6485c5c5d3df9e56b917521b4
Robin Hansma <robin@debsolutions.nl>
2015-05-06 09:02:35 +0200

    Fixed routes, so that goals can be added

--- public/modules/goals/config/goals.client.routes.js
+++ public/modules/goals/config/goals.client.routes.js
@@ -5,6 +5,14 @@ angular.module('goals').config(['$stateProvider',
 	function($stateProvider) {
 		// Goals state routing
 		$stateProvider.
+    state('listGoals', {
+      url: '/goals',
+      templateUrl: 'modules/goals/views/list-goals.client.view.html'
+    }).
+    state('createGoal', {
+      url: '/goals/create',
+      templateUrl: 'modules/goals/views/create-goal.client.view.html'
+    }).
 		state('viewGoal', {
 			url: '/goals/:goalId',
 			templateUrl: 'modules/goals/views/view-goal.client.view.html'
@@ -12,14 +20,6 @@ angular.module('goals').config(['$stateProvider',
 		state('editGoal', {
 			url: '/goals/:goalId/edit',
 			templateUrl: 'modules/goals/views/edit-goal.client.view.html'
-		}).
-		state('createGoal', {
-			url: '/goals/create',
-			templateUrl: 'modules/goals/views/create-goal.client.view.html'
-		}).
-		state('listGoals', {
-			url: '/goals',
-			templateUrl: 'modules/goals/views/list-goals.client.view.html'
 		});
 	}
 ]);
