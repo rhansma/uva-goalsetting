b9e3b763b1fd0db354421db9e01b1e90ca6958e5
Robin Hansma <robinhansma@outlook.com>
2015-05-08 15:45:55 +0200

    Use foundation instead of bootstrap

--- public/application.js
+++ public/application.js
@@ -17,4 +17,6 @@ angular.element(document).ready(function() {
 
 	//Then init the app
 	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
+  $(document).foundation();
+  //angular.foundation(document, [ApplicationConfiguration.applicationModuleName]);
 });
