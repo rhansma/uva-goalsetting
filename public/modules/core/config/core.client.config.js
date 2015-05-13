c773598e82cdba3aa0c2a0d9b12e6e68a176877c
Robin Hansma <robinhansma@outlook.com>
2015-05-13 13:28:52 +0200

    Menu converted to foundation, only left menu is not working now

---
+++ public/modules/core/config/core.client.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Robin on 12-5-2015.
+ */
+
+angular.module('core').run(['$rootScope',
+  function($rootScope) {
+    $rootScope.$on('$viewContentLoaded', function () {
+      $(document).foundation();
+    });
+  }
+]);
